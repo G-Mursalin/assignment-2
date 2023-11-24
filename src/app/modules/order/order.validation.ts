c0defd880e818025967c183e887358a24716c30d
Golam Mursalin <golammursalin309@gmail.com>
2023-11-24 19:47:03 +0600

    some zod valiation added for data

--- src/app/modules/order/order.validation.ts
+++ src/app/modules/order/order.validation.ts
@@ -1,9 +1,18 @@
 import { z } from 'zod';
 
 export const Order = z.object({
-  productName: z.string(),
-  price: z.number(),
-  quantity: z.number(),
+  productName: z
+    .string()
+    .trim()
+    .refine((val) => val.trim().length > 0, {
+      message: 'Please provide a  valid product name',
+    }),
+  price: z
+    .number()
+    .positive({ message: 'Price can not be negative number or zero' }),
+  quantity: z.number().refine((val) => val > 0, {
+    message: 'Quantity should be minimum 1',
+  }),
 });
 
 const OrderSchema = z.object({
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,20 +2,52 @@ import { z } from 'zod';
 
 const UserSchema = z.object({
   userId: z.number(),
-  username: z.string().trim(),
+  username: z
+    .string()
+    .trim()
+    .refine((val) => val.trim().length >= 3, {
+      message: 'User name should be minimum 3 character long',
+    }),
   password: z.string(),
   fullName: z.object({
-    firstName: z.string().trim(),
-    lastName: z.string().trim(),
+    firstName: z
+      .string()
+      .trim()
+      .refine((val) => val.trim().length > 0, {
+        message: 'Please provide a valid first name',
+      }),
+    lastName: z
+      .string()
+      .trim()
+      .refine((val) => val.trim().length > 0, {
+        message: 'Please provide a valid last name',
+      }),
   }),
-  age: z.number(),
+  age: z
+    .number()
+    .positive({ message: 'Age can not be negative number or zero' }),
   email: z.string().email('Invalid email address'),
   isActive: z.boolean().optional().default(true),
   hobbies: z.array(z.string()),
   address: z.object({
-    street: z.string().trim(),
-    city: z.string().trim(),
-    country: z.string().trim(),
+    street: z
+      .string()
+      .trim()
+      .refine((val) => val.trim().length > 0, {
+        message: 'Please tell us your valid street name',
+      }),
+    city: z
+      .string()
+      .trim()
+      .refine((val) => val.trim().length > 0, {
+        message: 'Please tell us your valid city name',
+      }),
+    country: z
+      .string()
+      .trim()
+      .refine((val) => val.trim().length > 0, {
+        message: 'Please tell us your valid country name',
+      }),
   }),
 });
 
