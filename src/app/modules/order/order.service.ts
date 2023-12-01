39a79e90ceb72dcfcbcd947f311755731c088bc3
Golam Mursalin <golammursalin309@gmail.com>
2023-12-01 14:13:11 +0600

    bug fixed

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -2,12 +2,12 @@ import { TOrders } from './order.interface';
 import OrderModel from './order.model';
 
 // Services for add orders
-const addOrders = async (userId: string, order: TOrders): Promise<TOrders> => {
+const addOrders = async (userId: string, order: TOrders) => {
   //Check if the 'orders' property already exists for this user, append a new product to it orders
   const existsOrder = await OrderModel.isThisUserOrderExists(Number(userId));
   if (existsOrder && existsOrder.orders.length >= 1) {
     return await OrderModel.updateOne(
-      { userId: userId },
+      { userId: Number(userId) },
       { $addToSet: { orders: order } },
     );
   } else {
