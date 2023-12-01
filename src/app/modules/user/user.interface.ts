c3c0e18f210c39ffbb7e22d761c651e9651dd5db
Golam Mursalin <golammursalin309@gmail.com>
2023-12-01 12:48:20 +0600

    bug fixed

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,10 +1,8 @@
 import { z } from 'zod';
-import { studentValidators } from './user.validation';
+import { userValidators } from './user.validation';
 import { Model } from 'mongoose';
 
-export type TUser = z.infer<
-  typeof studentValidators.createUserValidationSchema
->;
+export type TUser = z.infer<typeof userValidators.createUserValidationSchema>;
 
 export interface UserModelWithStaticMethod extends Model<TUser> {
   // eslint-disable-next-line no-unused-vars
