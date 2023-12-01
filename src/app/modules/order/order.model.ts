351b4e53c6a8fcfbe992717251d92871c810494d
Golam Mursalin <golammursalin309@gmail.com>
2023-12-01 18:00:51 +0600

    final touch

--- src/app/modules/order/order.model.ts
+++ src/app/modules/order/order.model.ts
@@ -2,9 +2,23 @@ import { Schema, model } from 'mongoose';
 import { TOrders, TOrder, OrderModelWithStaticMethod } from './order.interface';
 
 const order = new Schema<TOrder>({
-  productName: { type: String, required: true },
-  price: { type: Number, required: true },
-  quantity: { type: Number, required: true },
+  productName: { type: String, required: [true, 'Product name is required'] },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+    validate: {
+      validator: (value: number) => value > 0,
+      message: 'Price must be greater than zero',
+    },
+  },
+  quantity: {
+    type: Number,
+    required: [true, 'Quantity is required'],
+    validate: {
+      validator: (value: number) => value > 0,
+      message: 'Quantity must be greater than zero',
+    },
+  },
 });
 
 const orderSchema = new Schema<TOrders, OrderModelWithStaticMethod>({
